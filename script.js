92ceb76008afbc676da40211b4fb5d922b918ee2
Tripp's Repo <tripp.parham@me.com>
2019-11-29 03:23:23 -0500

    CSS added

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // ON CLICK EVENT FOR START BUTTON TO LOAD A QUESTION AND HIDE THE START BUTTON 
 $(".btn-dark").on("click", function() {
+    
     // remove button from view 
-    $(".btn-dark").remove();
+    $(".card").remove();
     // play theme song
     //$("#theme")[0].play(); 
     // display some text while the "game loads"
-    $(".ready").html("<h1> Get Ready!!</h1>")
+    //$(".ready").html("<h1> Get Ready!!</h1>")
     // log to console
     console.log("user clicked start");
+    //window.location.href='questions.html';
     // set timeout for theme music so it doesn't play the whole time
     setTimeout(function(){
         quizQuestion.getQuestion()
-        $("#theme")[0].pause(); 
-    }, 4500);
+        //$("#theme")[0].pause(); 
+    },0);
+    
 
 })
 
@@ -64,54 +67,54 @@ var quizQuestion = {
             // question text
             questionText: "Inside which HTML element do we put the JavaScript?",
             // question answers array
-            questionAnswer: ["<scripting>", "<script>", "<js>", "<javascript>"],
+            questionAnswer: ["1.  <scripting>", "2.  <script>", "3.  <js>", "4.  <javascript>"],
             // correct answer 
-            answer: "<script>"
+            answer: "2.  <script>"
         },
         {
             questionText: "Where is the correct place to insert a JavaScript?",
-            questionAnswer: ["Both in the <head> section and the <body> section are correct", "The <head> section", "The <body> section", "The <javascript> section"],
-            answer: "Both in the <head> section and the <body> section are correct"
+            questionAnswer: ["1.  Both in the <head> section and the <body> section are correct", "2.  The <head> section", "3.  The <body> section", "4.  The <javascript> section"],
+            answer: "1.  Both in the <head> section and the <body> section are correct"
         },
         {
             questionText: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-            questionAnswer: ['<script name="xxx.js">' , '<script src="xxx.js">' , '<script href="xxx.js">' , '<script call="xxx.js">'],
-            answer: '<script src="xxx.js">',
+            questionAnswer: ['1.  <script name="xxx.js">' , '2.  <script src="xxx.js">' , '3.  <script href="xxx.js">' , '4.  <script call="xxx.js">'],
+            answer: '2.  <script src="xxx.js">',
         },
         {
             questionText: 'How do you write "Hello World" in an alert box?',
-            questionAnswer: ['msg("Hello World");', 'msgBox("Hello World");', 'alert("Hello World");', 'alertBox("Hello World");'],
-            answer: 'alert("Hello World");',
+            questionAnswer: ['1.  msg("Hello World");', '2.  msgBox("Hello World");', '3.  alert("Hello World");', '4.  alertBox("Hello World");'],
+            answer: '3.  alert("Hello World");',
         },
         {
             questionText: "How do you create a function in JavaScript?",
-            questionAnswer: ["function myFunction[]", "function = myFunction()", "function:myFunction()", "function myFunction()"],
-            answer: "function = myFunction()",
+            questionAnswer: ["1.  function myFunction[]", "2.  function = myFunction()", "3.  function:myFunction()", "4.  function myFunction()"],
+            answer: "2.  function = myFunction()",
         },
         {
             questionText: 'How do you call a function named "myFunction"?',
-            questionAnswer: ["call function myFunction()", "call myFunction()", "myFunction()", "function.myFunction()"],
-            answer: "myFunction()",
+            questionAnswer: ["1.  call function myFunction()", "2.  call myFunction()", "3.  myFunction()", "4.  function.myFunction()"],
+            answer: "3.  myFunction()",
         },
         {
             questionText: "How to write an IF statement in JavaScript?",
-            questionAnswer: ["if(i==5)", "if i==5 then", "if i = 5", "if i = 5 then"],
-            answer: "if(i==5)",
+            questionAnswer: ["1.  if(i==5)", "2.  if i==5 then", "3.  if i = 5", "4.  if i = 5 then"],
+            answer: "1.  if(i==5)",
         },
         {
             questionText: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
-            questionAnswer: ["if(i <> 5)", "if i <> 5", "if i =! 5 then", "if (i != 5)"],
-            answer: "if (i != 5)",
+            questionAnswer: ["1.  if(i <> 5)", "2.  if i <> 5", "3.  if i =! 5 then", "4.  if (i != 5)"],
+            answer: "4.  if (i != 5)",
         },
         {
             questionText: "How does a WHILE loop start?",
-            questionAnswer: ["while (i <= 10)", "while i = 1 to 10", "while(i <= 10; i++)", " while i <= 10"],
-            answer: "while (i <= 10)",
+            questionAnswer: ["1.  while (i <= 10)", "2.  while i = 1 to 10", "3.  while(i <= 10; i++)", "4.  while i <= 10"],
+            answer: "1.  while (i <= 10)",
         },
         {
             questionText: "How can you add a comment in a JavaScript?",
-            questionAnswer: ["<!--This is a comment-->", '"This is a comment"', "//This is a comment", "***This is a comment***"],
-            answer: "//This is a comment",
+            questionAnswer: ["1.  <!--This is a comment-->", '"2.  This is a comment"', "3.  //This is a comment", "4.  ***This is a comment***"],
+            answer: "3.  //This is a comment",
         },
     ], 
 
@@ -227,7 +230,7 @@ var quizQuestion = {
             else {
                 quizQuestion.finalPage(); 
             }
-        }, 3000
+        }, 1000
         )
         
     }, 
